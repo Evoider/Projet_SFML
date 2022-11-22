66419487db973cd45f4fc4254f5594dd67a5698d
AuraXelus <come.santin@gmail.com>
2022-11-22 14:30:28 +0100

    Parce que

--- Projet_SFML/Main.cpp
+++ Projet_SFML/Main.cpp
@@ -1,10 +1,17 @@
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(500,500), "SFML works!", sf::Style::Default);
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    sf::Texture texture;
+    texture.loadFromFile("ressources/sprites/pika_pika.png");
+    sf::Sprite sprite;
+    sprite.scale(sf::Vector2f(1.f, 1.f)); // facters d'échelle relatifs à l'échelle actuelle
+    sprite.setTexture(texture);
 
     while (window.isOpen())
     {
@@ -14,11 +21,9 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
-
     return 0;
 }
