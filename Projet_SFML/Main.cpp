6dd7db91cbaa621b597c111527798bf03bec7c4f
AuraXelus <come.santin@gmail.com>
2022-11-22 15:41:58 +0100

    Sprite

--- Projet_SFML/Main.cpp
+++ Projet_SFML/Main.cpp
@@ -13,15 +13,43 @@ int main()
     sprite.scale(sf::Vector2f(1.f, 1.f)); // facters d'échelle relatifs à l'échelle actuelle
     sprite.setTexture(texture);
 
+    sf::Texture texture2;
+    texture2.loadFromFile("ressources/sprites/gravity_falls.png");
+    sf::Sprite character;
+    character.scale(sf::Vector2f(1.f, 1.f)); // facters d'échelle relatifs à l'échelle actuelle
+    character.setTexture(texture2);
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Z)
+                {
+                    sprite.move(0.f, -6.f);
+                }
+                if (event.key.code == sf::Keyboard::S)
+                {
+                    sprite.move(0.f, 6.f);
+                }
+                if (event.key.code == sf::Keyboard::Q)
+                {
+                    sprite.move(-6.f, 0.f);
+                }
+                if (event.key.code == sf::Keyboard::D)
+                {
+                    sprite.move(6.f, 0.f);
+                }
+            }
         }
         window.clear();
+        window.draw(character);
         window.draw(sprite);
         window.display();
     }
