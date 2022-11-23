6c6df25cb27dfb6d6a5e76fe3ce96d3bd924e37c
Ever <yrocher@gaming.tech>
2022-11-23 11:48:01 +0100

    fichier conf

--- Projet_SFML/Pokemon.cpp
+++
@@ -1,8 +0,0 @@
-#include "Pokemon.h"
-
-Pokemon::Pokemon(std::string File, std::string type,int pv, int att, int def, int vit)
-{
-	texture.loadFromFile(File);
-	sprite.setTexture(texture);
-	m_type = type;
-}
