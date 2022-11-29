3efcb1375275e3d1596d532c5b5b3d285813f99b
AuraXelus <come.santin@gmail.com>
2022-11-29 16:44:53 +0100

    ENFIN

--- Projet_SFML/Background.cpp
+++ Projet_SFML/Background.cpp
@@ -12,109 +12,120 @@ Background::Background()
 
 void Background::initTab()
 {
-	int tabmap[20][30] =
+	int tabmap[40][40] =
 	{
-		{13,0,0,0,0,0,13,0,0,0,0,0,13,0,0,0,0,0,13,0,0,0,0,0,13,0,0,0,0,0},	//Forêt
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		{13,0,0,0,0,14,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0,99,99,99,99,99,99,99,99,99,99,99,99,99,0,0,0,0,0,0,0}, //Fin forêt haut
-		{0,0,0,0,0,0,0,0,0,0,99,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		  {0,0,0,0,0,0,0,0,0,99,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		{13,0,0,0,0,14,0,0,0,99,0,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		   {0,0,0,0,0,0,0,0,0,0,0,0,4,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		   {0,0,0,0,0,0,0,0,0,0,0,0,4,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		   {0,0,0,0,0,0,0,0,0,0,0,0,4,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		   {0,0,0,0,0,0,0,0,0,0,0,0,4,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		{0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		{0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+        {11, 12, 13, 14, 15, 16, 11, 12, 13, 14, 15, 16, 11, 12, 13, 14, 15, 16, 11, 12, 13, 14, 15, 16, 11, 12, 13, 14, 15, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {32, 33, 34, 35, 36, 37, 32, 33, 34, 35, 36, 37, 32, 33, 34, 35, 36, 37, 32, 33, 34, 35, 36, 37, 32, 33, 34, 35, 36, 37, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {53, 54, 55, 56, 57, 58, 53, 54, 55, 56, 57, 58, 53, 54, 55, 56, 57, 58, 53, 54, 55, 56, 57, 58, 53, 54, 55, 56, 57, 58, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {74, 75, 76, 77, 78, 79, 74, 75, 76, 77, 78, 79, 74, 75, 76, 77, 78, 79, 74, 75, 76, 77, 78, 79, 74, 75, 76, 77, 78, 79, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {95, 96, 97, 98, 99, 100, 95, 96, 97, 98, 99, 100, 95, 96, 97, 98, 99, 100, 95, 96, 97, 98, 99, 100, 95, 96, 97, 98, 99, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {116, 117, 118, 119, 120, 121, 116, 117, 118, 119, 120, 121, 116, 117, 118, 119, 120, 121, 116, 117, 118, 119, 120, 121, 116, 117, 118, 119, 120, 121, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 18, 19, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 40, 40, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 40, 40, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 40, 40, 102, 19, 19, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 40, 40, 40, 40, 40, 40, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 40, 40, 40, 40, 40, 40, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 60, 61, 61, 61, 82, 40, 40, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 40, 40, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 40, 40, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 60, 61, 61, 62, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 
 	};
 
 	
 
-	for (int y = 0; y < 20 ; y++)
+	for (int y = 0; y < 40 ; y++)
 	{
-		for (int x = 0; x < 30; x++)
+		for (int x = 0; x < 40; x++)
 		{
-			if (tabmap[y][x] == 1)	
-			{
-				sf::Sprite pass;
-				pass.setTexture(forest);
-				pass.setTextureRect(sf::IntRect (272 + 16, 0 + 16, 16, 16));
-				pass.scale(4.6875, 4.6875);
-				pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
-				vecbox.push_back(pass);
-			}
-			//1-Chemin millieu
-			if (tabmap[y][x] == 2)	
-			{
-				sf::Sprite pass;
-				pass.setTexture(forest);
-				pass.setTextureRect(sf::IntRect(272 + 16, 0, 16, 16));
-				pass.scale(4.6875, 4.6875);
-				pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
-				vecbox.push_back(pass);
-			}
-			//2-Chemin haut
-			if (tabmap[y][x] == 3)	
-			{
-				sf::Sprite pass;
-				pass.setTexture(forest);
-				pass.setTextureRect(sf::IntRect(272 + 16, 0 + 16 + 16, 16, 16));
-				pass.scale(4.6875, 4.6875);
-				pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
-				vecbox.push_back(pass);
-			}
-			//3-Chemin bas
-			if (tabmap[y][x] == 4)
-			{
-				sf::Sprite pass;
-				pass.setTexture(forest);
-				pass.setTextureRect(sf::IntRect(272, 0 + 16, 16, 16));
-				pass.scale(4.6875, 4.6875);
-				pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
-				vecbox.push_back(pass);
-			}
-			//4-Chemin gauche
-			if (tabmap[y][x] == 5)
-			{
-				sf::Sprite pass;
-				pass.setTexture(forest);
-				pass.setTextureRect(sf::IntRect(272 + 16 + 16, 0 + 16, 16, 16));
-				pass.scale(4.6875, 4.6875);
-				pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
-				vecbox.push_back(pass);
-			}
-			//5-Chemin droite
-			if (tabmap[y][x] == 13)
-			{
-				sf::Sprite pass;
-				pass.setTexture(forest);
-				pass.setTextureRect(sf::IntRect(160, 0, 96, 96));
-				pass.scale(4.6875, 4.6875);
-				pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
-				vecbox.push_back(pass);
-			}
-			//13- Forêt
-			if (tabmap[y][x] == 14)
-			{
-				sf::Sprite pass;
-				pass.setTexture(forest);
-				pass.setTextureRect(sf::IntRect(160 + 16, 0, 82, 96));  //2 arbres forêt et demi + 2 pixels
-				pass.scale(4.6875, 4.6875);
-				pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
-				vecbox.push_back(pass);
-			}
-			//14- Forêt fin droite
+            for (int m = 0; m < 1000; m++)
+            {
+                if (tabmap[y][x] - 1 == m)
+                {
+                    if (m <= 20)
+                    {
+                        sf::Sprite box;
+                        box.setTexture(forest);
+                        box.setTextureRect(sf::IntRect((m % 21) * 16, 0, 16, 16));
+                        box.scale(4.6875, 4.6875);
+                        box.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+                        vecbox.push_back(box);
+                    }
+                    else if (m <= 41)
+                    {
+                        sf::Sprite box;
+                        box.setTexture(forest);
+                        box.setTextureRect(sf::IntRect((m % 21) * 16, 16, 16, 16));
+                        box.scale(4.6875, 4.6875);
+                        box.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+                        vecbox.push_back(box);
+                    }
+                    else if (m <= 62)
+                    {
+                        sf::Sprite box;
+                        box.setTexture(forest);
+                        box.setTextureRect(sf::IntRect((m % 21) * 16, 2*16, 16, 16));
+                        box.scale(4.6875, 4.6875);
+                        box.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+                        vecbox.push_back(box);
+                    }
+                    else if (m <= 83)
+                    {
+                        sf::Sprite box;
+                        box.setTexture(forest);
+                        box.setTextureRect(sf::IntRect((m % 21) * 16, 3*16, 16, 16));
+                        box.scale(4.6875, 4.6875);
+                        box.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+                        vecbox.push_back(box);
+                    }
+                    else if (m <= 104)
+                    {
+                        sf::Sprite box;
+                        box.setTexture(forest);
+                        box.setTextureRect(sf::IntRect((m % 21) * 16, 4*16, 16, 16));
+                        box.scale(4.6875, 4.6875);
+                        box.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+                        vecbox.push_back(box);
+                    }
+                    else if (m <= 125)
+                    {
+                        sf::Sprite box;
+                        box.setTexture(forest);
+                        box.setTextureRect(sf::IntRect((m % 21) * 16, 5*16, 16, 16));
+                        box.scale(4.6875, 4.6875);
+                        box.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+                        vecbox.push_back(box);
+                    }
+                }
+            }
 		}
 	}
+
 }
 
 void Background::render(sf::RenderTarget* target)
@@ -124,3 +135,75 @@ void Background::render(sf::RenderTarget* target)
 		target->draw(this->vecbox[i]);
 	}
 }
+
+
+//if (tabmap[y][x] == 1)
+//{
+//	sf::Sprite pass;
+//	pass.setTexture(forest);
+//	pass.setTextureRect(sf::IntRect(272 + 16, 0 + 16, 16, 16));
+//	pass.scale(4.6875, 4.6875);
+//	pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+//	vecbox.push_back(pass);
+//}
+////1-Chemin millieu
+//if (tabmap[y][x] == 2)
+//{
+//	sf::Sprite pass;
+//	pass.setTexture(forest);
+//	pass.setTextureRect(sf::IntRect(272 + 16, 0, 16, 16));
+//	pass.scale(4.6875, 4.6875);
+//	pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+//	vecbox.push_back(pass);
+//}
+////2-Chemin haut
+//if (tabmap[y][x] == 3)
+//{
+//	sf::Sprite pass;
+//	pass.setTexture(forest);
+//	pass.setTextureRect(sf::IntRect(272 + 16, 0 + 16 + 16, 16, 16));
+//	pass.scale(4.6875, 4.6875);
+//	pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+//	vecbox.push_back(pass);
+//}
+////3-Chemin bas
+//if (tabmap[y][x] == 4)
+//{
+//	sf::Sprite pass;
+//	pass.setTexture(forest);
+//	pass.setTextureRect(sf::IntRect(272, 0 + 16, 16, 16));
+//	pass.scale(4.6875, 4.6875);
+//	pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+//	vecbox.push_back(pass);
+//}
+////4-Chemin gauche
+//if (tabmap[y][x] == 5)
+//{
+//	sf::Sprite pass;
+//	pass.setTexture(forest);
+//	pass.setTextureRect(sf::IntRect(272 + 16 + 16, 0 + 16, 16, 16));
+//	pass.scale(4.6875, 4.6875);
+//	pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+//	vecbox.push_back(pass);
+//}
+////5-Chemin droite
+//if (tabmap[y][x] == 13)
+//{
+//	sf::Sprite pass;
+//	pass.setTexture(forest);
+//	pass.setTextureRect(sf::IntRect(160, 0, 96, 96));
+//	pass.scale(4.6875, 4.6875);
+//	pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+//	vecbox.push_back(pass);
+//}
+////13- Forêt
+//if (tabmap[y][x] == 14)
+//{
+//	sf::Sprite pass;
+//	pass.setTexture(forest);
+//	pass.setTextureRect(sf::IntRect(160 + 16, 0, 82, 96));  //2 arbres forêt et demi + 2 pixels
+//	pass.scale(4.6875, 4.6875);
+//	pass.setPosition(sf::Vector2f(x * offSetX, y * offSetY));
+//	vecbox.push_back(pass);
+//}
+////14- Forêt fin droite
